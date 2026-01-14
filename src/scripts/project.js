fc2b1c7e76b6c970cf970c7f2809737b2fa64996
drawdlu <152858099+drawdlu@users.noreply.github.com>
2026-01-14 20:01:04 +0800

    feat: Create projects that will hold todo lists

---
+++ src/scripts/project.js
@@ -0,0 +1,21 @@
+function createProject(projectName) {
+    let name = projectName;
+    let list = [];
+
+    const editName = (newName) => { name = newName };
+
+    const addItem = (newToDo) => { list.push(newToDo) };
+
+    const removeItem = (toDoItem) => {
+        const itemIndex = list.indexOf(toDoItem);
+        list.splice(itemIndex, 1);
+    }
+    
+    return {
+        get name () { return name; },
+        get list() { return [...list] },
+        editName,
+        addItem,
+        removeItem
+    };
+}
