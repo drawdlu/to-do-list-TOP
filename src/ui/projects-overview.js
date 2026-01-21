7ae5b2b6aecb2f8288635ba739077695050d3f1c
drawdlu <152858099+drawdlu@users.noreply.github.com>
2026-01-21 18:29:56 +0800

    feat: Create projects overview page

---
+++ src/ui/projects-overview.js
@@ -0,0 +1,25 @@
+import { createContainer, createList, createItemName, createButtonField } from "./helpers";
+
+export default function createProjectsOverview(projectList) {
+    const container = createContainer("project-overview");
+    const list = createList("project-list", "project-card", projectList, appendToList)
+
+    container.append(list);
+
+    return list;
+}
+
+function appendToList(ul, item, containerName) {
+    const itemDiv = createListItem(item.name, containerName);
+    ul.append(itemDiv);
+}
+
+function createListItem(name, containerName) {
+    const itemContainer = createContainer(containerName);
+    const itemName = createItemName(name);
+    const deleteButton = createButtonField("delete", "Delete");
+
+    itemContainer.append( itemName, deleteButton);
+
+    return itemContainer;
+}
