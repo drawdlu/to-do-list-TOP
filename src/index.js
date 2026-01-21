b41c3cab38c829777b6e35fe1a721c96d11d3f45
drawdlu <152858099+drawdlu@users.noreply.github.com>
2026-01-21 21:05:08 +0800

    feat: Load navigation link pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { format } from "date-fns";
 
 import createProject from "./modules/project";
 import createToDoItem from "./modules/todo";
+import createProjectsOverview from "./ui/projects-overview";
+import createProjectPage from "./ui/project-page";
+import createProjectForm from "./ui/project-form";
 import { events } from "./modules/pubsub";
 import { addProject } from "./ui/navigation";
 
@@ -23,3 +26,58 @@ function createDefaultProject() {
 }
 
 createDefaultProject();
+
+const nav = document.querySelector("nav");
+const pages = {
+    "projects": createProjectsOverview(projectList),
+    "create-project": createProjectForm()
+}
+
+nav.addEventListener("click", e => {
+    const navTarget = e.target;
+
+    if (navTarget.nodeName === "BUTTON") {
+        findAndLoadPage(navTarget);
+    }
+})
+
+function findAndLoadPage(button) {
+    if (button.classList.contains("project-link")) {
+        const project = findProject(button.textContent);
+        loadProjectPage(project);
+    } else {
+        loadNavPage(button)
+    }
+}
+
+function loadNavPage(button) {
+    const pageId = button.dataset.page;
+    const contentDiv = getContentDiv();
+    const page = pages[pageId];
+
+    clearDiv(contentDiv);
+    contentDiv.append(page);
+}
+
+function loadProjectPage(project) {
+    const contentDiv = getContentDiv();
+    const projectPage = createProjectPage(project);
+
+    clearDiv(contentDiv);
+    contentDiv.append(projectPage);
+}
+
+function getContentDiv() {
+    return document.querySelector("div.content");
+}
+
+function clearDiv(div) {
+    div.textContent = "";
+}
+
+function findProject(text) {
+    const projectListNames = projectList.map( (project) => project.name );
+    const index = projectListNames.indexOf(text)
+
+    return projectList[index]
+}
