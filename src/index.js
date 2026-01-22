4186ffbc7e3c37a501ecef67edb8627b8501e74b
drawdlu <152858099+drawdlu@users.noreply.github.com>
2026-01-22 18:46:48 +0800

    refactor: Separate page load logic form index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ import { format } from "date-fns";
 
 import createProject from "./modules/project";
 import createToDoItem from "./modules/todo";
-import createProjectsOverview from "./ui/projects-overview";
-import createProjectPage from "./ui/project-page";
-import createProjectForm from "./ui/project-form";
+import findAndLoadPage from "./ui/page-load";
 import { events } from "./modules/pubsub";
 import { addProject } from "./ui/navigation";
-
-events.on("createNewProject", addProject);
-events.on("createNewProject", addToList);
 
 const projectList = [];
 
 function addToList(project) {
     projectList.push(project);
 }
+
+events.on("createNewProject", addProject);
+events.on("createNewProject", addToList);
 
 function createDefaultProject() {
     const defaultProject = createProject("Default");
@@ -28,56 +26,11 @@ function createDefaultProject() {
 createDefaultProject();
 
 const nav = document.querySelector("nav");
-const pages = {
-    "projects": createProjectsOverview(projectList),
-    "create-project": createProjectForm()
-}
 
 nav.addEventListener("click", e => {
     const navTarget = e.target;
 
     if (navTarget.nodeName === "BUTTON") {
-        findAndLoadPage(navTarget);
+        findAndLoadPage(projectList, navTarget);
     }
-})
-
-function findAndLoadPage(button) {
-    if (button.classList.contains("project-link")) {
-        const project = findProject(button.textContent);
-        loadProjectPage(project);
-    } else {
-        loadNavPage(button)
-    }
-}
-
-function loadNavPage(button) {
-    const pageId = button.dataset.page;
-    const contentDiv = getContentDiv();
-    const page = pages[pageId];
-
-    clearDiv(contentDiv);
-    contentDiv.append(page);
-}
-
-function loadProjectPage(project) {
-    const contentDiv = getContentDiv();
-    const projectPage = createProjectPage(project);
-
-    clearDiv(contentDiv);
-    contentDiv.append(projectPage);
-}
-
-function getContentDiv() {
-    return document.querySelector("div.content");
-}
-
-function clearDiv(div) {
-    div.textContent = "";
-}
-
-function findProject(text) {
-    const projectListNames = projectList.map( (project) => project.name );
-    const index = projectListNames.indexOf(text)
-
-    return projectList[index]
-}
+});
---
+++ src/ui/page-load.js
@@ -0,0 +1,49 @@
+import createProjectsOverview from "./projects-overview";
+import createProjectPage from "./project-page";
+import createProjectForm from "./project-form";
+
+const pages = {
+    "projects": createProjectsOverview,
+    "create-project": createProjectForm
+}
+
+export default function findAndLoadPage(projectList, button) {
+    if (button.classList.contains("project-link")) {
+        const project = findProject(projectList, button.textContent);
+        loadProjectPage(project);
+    } else {
+        loadNavPage(projectList, button)
+    }
+}
+
+function loadNavPage(projectList, button) {
+    const pageId = button.dataset.page;
+    const contentDiv = getContentDiv();
+    const page = pages[pageId](projectList);
+
+    clearDiv(contentDiv);
+    contentDiv.append(page);
+}
+
+function loadProjectPage(project) {
+    const contentDiv = getContentDiv();
+    const projectPage = createProjectPage(project);
+
+    clearDiv(contentDiv);
+    contentDiv.append(projectPage);
+}
+
+function getContentDiv() {
+    return document.querySelector("div.content");
+}
+
+function clearDiv(div) {
+    div.textContent = "";
+}
+
+function findProject(projectList, text) {
+    const projectListNames = projectList.map( (project) => project.name );
+    const index = projectListNames.indexOf(text)
+
+    return projectList[index]
+}
